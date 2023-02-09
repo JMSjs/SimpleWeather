2651c10845304b8b76e58d8cddbf37f46bc93ed8
jmsjs <jmsong8@gmail.com>
2023-02-08 16:05:09 -0800

    simple api connected

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
+const apiEndPoint = config.API_END_POINT;
 const apiKEY = config.SECRET_API_KEY;
 
+let latitude = 47.608013
+let longitude = -122.335167
+
+const loadLocalWeather = async () => {
+    try {
+        const res = await fetch(apiEndPoint + "lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKEY);
+        const data = await res.json();
+        console.log(data); 
+        let tempFahrenheit = Math.round((data.main.temp - 273.15) * 9/5 + 32);
+        console.log(`The current weather in ${data.name}, ${data.sys.country} is ${data.weather[0].description}, and feels like ${tempFahrenheit}\u00B0 F.`);
+
+    } catch (err) {
+        console.log ("Error!!", err)
+    }
+};
+
+loadLocalWeather();
